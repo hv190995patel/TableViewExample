7b25e53f909958e3275792e3e8e98e1e237fe210
Hiten Patel <Hiten@hitens-macbook-pro.local>
2019-02-27 08:56:15 -0500

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Hiten Patel on 2019-02-27.
+//  Copyright © 2019 MAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
